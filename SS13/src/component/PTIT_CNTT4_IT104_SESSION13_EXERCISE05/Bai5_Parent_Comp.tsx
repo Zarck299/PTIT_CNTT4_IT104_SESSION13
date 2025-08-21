05a988ab0000498af1a50fdb2fe254f648a43a5b
Anh <honganhzarck@gmail.com>
2025-08-21 10:07:59 +0700

    SS13

--- SS13/src/component/PTIT_CNTT4_IT104_SESSION13_EXERCISE05/Bai5_Parent_Comp.tsx
+++ SS13/src/component/PTIT_CNTT4_IT104_SESSION13_EXERCISE05/Bai5_Parent_Comp.tsx
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
+import { Component } from "react";
 import Children_Comp from "./Bai5_Children_Comp";
 
-const Parent_Comp: React.FC = () => {
-  const [product] = useState({
-    id: 1,
-    name: "Bưởi ba roi",
-    price: "12.000 ₫",
-    quantity: 6,
-  });
+interface Product {
+  id: number;
+  name: string;
+  price: string;
+  quantity: number;
+}
 
-  return (
-    <div>
-      <h2>Component Cha</h2>
-      <Children_Comp product={product} />
-    </div>
-  );
-};
+interface State {
+  product: Product;
+}
 
-export default Parent_Comp;
+export default class Parent_Comp extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      product: {
+        id: 1,
+        name: "Bưởi ba roi",
+        price: "12.000 ₫",
+        quantity: 6,
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Component Cha</h2>
+        <Children_Comp product={this.state.product} />
+      </div>
+    );
+  }
+}
